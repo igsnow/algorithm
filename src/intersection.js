b9cec09864e14545f679c09343fb74a4be8e84b9
igsnow <zzy716810918@gmail.com>
2019-01-23 15:34:48 +0800

    两个数组的交集

---
+++ src/intersection.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+  let arr = []
+  for (let i = 0; i < nums2.length; i++) {
+    if (nums1.indexOf(nums2[i]) > -1) {
+      arr.push(nums2[i])
+    }
+  }
+  return [...new Set(arr)]
+};
+
+let nums1 = [1, 2, 2, 1], nums2 = [2, 2, 3, 1, 1, 1, 1, 1]
+let res = intersection(nums1, nums2)
+console.log(res)
