ad2f073c7fa8d6cf758a05cf8736768a9f50d721
igsnow <zzy716810918@gmail.com>
2019-01-23 15:55:41 +0800

    优化两个数组的交集

--- src/intersection.js
+++ src/intersection.js
@@ -4,13 +4,18 @@
  * @return {number[]}
  */
 var intersection = function (nums1, nums2) {
-  let arr = []
-  for (let i = 0; i < nums2.length; i++) {
-    if (nums1.indexOf(nums2[i]) > -1) {
-      arr.push(nums2[i])
-    }
-  }
-  return [...new Set(arr)]
+  return [... new Set(nums1.filter(item => nums2.includes(item)))]
+
+  // return [... new Set(nums1.filter(item => nums2.indexOf(item) > -1))]
+
+
+  // let arr = []
+  // for (let i = 0; i < nums2.length; i++) {
+  //   if (nums1.indexOf(nums2[i]) > -1) {
+  //     arr.push(nums2[i])
+  //   }
+  // }
+  // return [...new Set(arr)]
 };
 
 let nums1 = [1, 2, 2, 1], nums2 = [2, 2, 3, 1, 1, 1, 1, 1]
